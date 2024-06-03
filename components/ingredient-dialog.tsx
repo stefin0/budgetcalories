e4c370c3a603de151fefd6c7d36cfa2fb8f38f3f
stefin <stefinracho@gmail.com>
2024-06-02 21:23:05 -0700

    initial setup of ingredient form and carousel

---
+++ components/ingredient-dialog.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import IngredientForm from "./ingredient-form";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { useState } from "react";
+
+export default function IngredientDialog() {
+  const [api, setApi] = useState<CarouselApi>();
+
+  function handleNextSlide() {
+    api?.scrollNext();
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Cook</Button>
+      </DialogTrigger>
+      <DialogContent className="w-[95vw] max-w-[425px] rounded-lg">
+        <Carousel setApi={setApi} opts={{watchDrag: false}}>
+          <CarouselContent>
+            <CarouselItem>
+              <DialogHeader className="text-left">
+                <DialogTitle>Create Ingredient</DialogTitle>
+                <DialogDescription>
+                  Give information for your ingredient.
+                </DialogDescription>
+              </DialogHeader>
+              <div className="grid gap-1">
+                <IngredientForm handleNextSlide={handleNextSlide} />
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <DialogHeader className="text-left">
+                <DialogTitle>Review</DialogTitle>
+                <DialogDescription>
+                  Nutrition facts for 1 cup Milk.
+                </DialogDescription>
+              </DialogHeader>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </DialogContent>
+    </Dialog>
+  );
+}
---
+++ components/ingredient-form.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Name must be at least 2 characters.",
+    })
+    .max(25, {
+      message: "Name cannot exceed 25 characters.",
+    }),
+});
+
+export default function IngredientForm({
+  handleNextSlide,
+}: {
+  handleNextSlide: () => void;
+}) {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+    handleNextSlide();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Next
+        </Button>
+      </form>
+    </Form>
+  );
+}
