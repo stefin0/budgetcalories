c941c9e82f4381188b49dbfc95d1ea0ab7258930
stefin <stefinracho@gmail.com>
2024-08-13 18:43:38 -0700

    fix font bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -7,7 +7,7 @@ import Navbar from "@/components/nav-bar";
 import { SessionProvider } from "next-auth/react";
 import { auth } from "@/auth";
 
-const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
+const inter = Inter({ subsets: ["latin"]});
 
 export const metadata: Metadata = {
   title: "Budget Calories",
@@ -24,7 +24,7 @@ export default async function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={cn("bg-background font-sans antialiased", fontSans.variable)}
+        className={cn("bg-background font-sans antialiased", inter.className)}
       >
         <SessionProvider session={session}>
           <ThemeProvider
